91553ea3adc71c3a96f75ca41f79700f6de56ba1
AjaySRawat07 <ajju246455@gmail.com>
2025-04-12 23:02:03 +0530

    backend_completed

--- server/src/controllers/connection/requestByOthers.js
+++ server/src/controllers/connection/requestByOthers.js
@@ -3,88 +3,96 @@ const UserModel = require("../../models/user.model");
 
 const USER_SAFE_EXE = "firstName lastName photoURL about skills ";
 
-const reqRecivedOfOthers = async(req,res) =>{
-    try{
-        const loggedInUser = req.user;
-        console.log(loggedInUser);
-        const connectionRequests = await connectionModel.find({
-            toUserId  : loggedInUser._id,
-            status : "interested",
-        }).populate("fromUserId", USER_SAFE_EXE)
-        // }).populate("fromUserId", ["firstName", "lastName"]) another method
-        res.json({
-            success : true,
-            message : "Data fetched successfully",
-            result : connectionRequests
-        })
-    }
-    catch(error){
-        console.error("Error in request recived pannel: ",error.message);
-    }
-}
+const reqRecivedOfOthers = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    console.log(loggedInUser);
+    const connectionRequests = await connectionModel
+      .find({
+        toUserId: loggedInUser._id,
+        status: "interested",
+      })
+      .populate("fromUserId", USER_SAFE_EXE);
+    // }).populate("fromUserId", ["firstName", "lastName"]) another method
+    res.json({
+      success: true,
+      message: "Data fetched successfully",
+      result: connectionRequests,
+    });
+  } catch (error) {
+    console.error("Error in request recived pannel: ", error.message);
+  }
+};
 
-const userFriendsSection = async(req,res) =>{
-    try{
-        const loggedInUser = req.user;
+const userFriendsSection = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-        const ConnectionRequest = await connectionModel.find({
-            $or : [
-                { toUserId : loggedInUser._id, status: "accepted"},
-                {fromUserId : loggedInUser._id, status : "accepted"},
-            ]
-        })
-        .populate("fromUserId", USER_SAFE_EXE)
-        .populate("toUserId" , USER_SAFE_EXE);
+    const ConnectionRequest = await connectionModel
+      .find({
+        $or: [
+          { toUserId: loggedInUser._id, status: "accepted" },
+          { fromUserId: loggedInUser._id, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", USER_SAFE_EXE)
+      .populate("toUserId", USER_SAFE_EXE);
 
-        const data = ConnectionRequest.map(row => {
-            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
-                return row.toUserId;
-            }
-            return row.fromUserId;
-        });
-        res.json({
-            success : "true",
-            message : "User connections fetched successfully",
-            data : data,
-        })
-    }
-    catch(error){
-        console.error("Error in Fried connections: ",error.message);
-    }
-}
+    const data = ConnectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({
+      success: "true",
+      message: "User connections fetched successfully",
+      data: data,
+    });
+  } catch (error) {
+    console.error("Error in Fried connections: ", error.message);
+  }
+};
 
-const userFeedContainer = async(req,res) =>{
-    try{
-        const loggedInUser = req.user;
+const userFeedContainer = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-        const connectionRequests = await connectionModel.find({
-            $or : [{fromUserId : loggedInUser._id}, {toUserId : loggedInUser._id}],
-        }).select("fromUserId toUserId");
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit; // formula for skip
 
-        // not adding those user in feed whom you already with any type of connection
-        const hideUserFromFeed = new Set();
-        connectionRequests.forEach((req) => {
-            hideUserFromFeed.add(req.fromUserId.toString());
-            hideUserFromFeed.add(req.toUserId.toString());
-        })
-        console.log(hideUserFromFeed);
+    const connectionRequests = await connectionModel
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      .select("fromUserId toUserId");
 
-        const user = await UserModel.find({
-            $and : [
-              { _id : {$nin : Array.from(hideUserFromFeed)}}, // $nin => not in this array
-              { _id : {$ne : loggedInUser._id}} // $ne => not a loggin user
-            ]
-        }).select(USER_SAFE_EXE);
+    // not adding those user in feed whom you already with any type of connection
+    const hideUserFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
 
-        res.send(user);
-    }
-    catch(error){
-        console.error("Error in userFeedContainer : ", error.message);
-    }
-}
+    const user = await UserModel.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } }, // $nin => not in this array
+        { _id: { $ne: loggedInUser._id } }, // $ne => not a loggin user
+      ],
+    })
+      .select(USER_SAFE_EXE)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(user);
+  } catch (error) {
+    console.error("Error in userFeedContainer : ", error.message);
+  }
+};
 
 module.exports = {
-    reqRecivedOfOthers,
-    userFriendsSection,
-    userFeedContainer
+  reqRecivedOfOthers,
+  userFriendsSection,
+  userFeedContainer,
 };
--- server/src/routes/connection/friend.reqs.js
+++ server/src/routes/connection/friend.reqs.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { userAuth } = require("../../middlewares/auth");
-const{ reqRecivedOfOthers, userFriendsSection, userFeedContainer }= require("../../controllers/connection/requestByOthers");
+const {
+  reqRecivedOfOthers,
+  userFriendsSection,
+  userFeedContainer,
+} = require("../../controllers/connection/requestByOthers");
 
-const usersRouter  = express.Router();
+const usersRouter = express.Router();
 
-usersRouter.get("/user/requests/received", userAuth , reqRecivedOfOthers);
+usersRouter.get("/user/requests/received", userAuth, reqRecivedOfOthers);
 
-usersRouter.post("/user/connections", userAuth , userFriendsSection);
+usersRouter.post("/user/connections", userAuth, userFriendsSection);
 
 usersRouter.get("/user/feed", userAuth, userFeedContainer);
 
